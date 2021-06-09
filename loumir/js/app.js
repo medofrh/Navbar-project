b370f6b8dfcd4f50fff1ed886ea42ae5a7b6c9a3
medofrh <medo0o0frh@gmail.com>
2021-06-09 21:03:10 +0200

    loumir site

---
+++ loumir/js/app.js
@@ -0,0 +1,44 @@
+$(document).ready(()=>{
+    $('.navbar').addClass('navbar-ready');
+
+    /*$(".navbar a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            },500, function(){
+            window.location.hash = hash;
+          });
+        }
+    });*/
+    //TODO: fix hash href onload and moveing to selected section
+    //TODO: change navbar color when sliding
+});
+$('.container-sec').scroll(function(){
+    $('section').each(function(){
+        if(isScrolledIntoView($(this))){
+            $('.navbar-collapse ul li a').removeClass('active');
+            $(`a[href$="${this.id}"]`).addClass('active');
+        }
+    });
+});
+
+function isScrolledIntoView(elem){
+
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+$('#navbarNav ul li a').click(function(){
+    $('.navbar-collapse ul li a').removeClass('active');
+    $(this).addClass('active');
+});
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
